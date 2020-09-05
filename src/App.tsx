42d10b216cfcccfe9c50711fb6dbdd0c49ad37f6
Daniel Mesquita <danielmesquitta123@gmail.com>
2020-09-05 18:10:08 -0300

    created project and configured root import

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+export default App;
